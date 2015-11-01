4407458737ec8c95664740fe61f0d98ccbd539cf
dylan-reeves <dylan.reeves@sabulk.co.za>
2015-11-01 12:35:36 +0200

    add twitter and migrate

---
+++ plantms/equipment/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class equipment(models.Model):
+	
+	docs
+
+	tring for equipment"models.Modelf __init__(self, arg)
+
+	:
+		super(equip
+			t,models.Model.__
+		t__()
+		self.arg = arg
+
+class site(models.Model):
+	name = models.CharField(max_length=100)
+	reportingManager = models.ForeignKey(User)
+	docstring for site"models.Modelf __init__(self, arg):
+		supename = models.CharField(max_length=100)
+		reportingManager = models.ForeignKey(r(site,
+		name = models.CharField(max_length=100)
+		reportingManager = models.ForeignKey(	els.Model.__init__
+				self.arg = arg
+		
